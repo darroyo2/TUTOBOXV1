ae4e0f18304daeb5ab24597e44862ea92960e225
darroyo2 <20202370@aloe.ulima.edu.pe>
2023-06-04 17:28:33 -0500

    Implements vacios curso, mensaje, publicacion

---
+++ TUTOBOX/src/main/java/dao/PublicacionDAOImpl.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import model.Publicacion;
+
+/**
+ *
+ * @author HP
+ */
+public class PublicacionDAOImpl implements PublicacionDAO{
+
+    @Override
+    public void insertar(Publicacion publicacion) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Publicacion publicacion) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Publicacion publicacion) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Publicacion> listar() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
