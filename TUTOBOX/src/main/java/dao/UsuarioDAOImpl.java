d3869cf03e0daa4b6aca1b1eafec4e56e49cea76
brayanpiere <brayanpiere@gmail.com>
2023-06-04 18:36:11 -0500

    UsuarioDaoImpl implementacion de metodos

--- TUTOBOX/src/main/java/dao/UsuarioDAOImpl.java
+++ TUTOBOX/src/main/java/dao/UsuarioDAOImpl.java
@@ -2,7 +2,9 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import model.Usuario;
 import util.ConexionUtil;
@@ -16,29 +18,58 @@ public class UsuarioDAOImpl implements UsuarioDAO {
 
     @Override
     public void insertarUsuario(Usuario usuario) throws SQLException {
-        String query = "INSERT INTO usuario (nombre, contrasena, correo, id_tipo) VALUES (?, ?, ?, ?)";
+        String query = "INSERT INTO `tutobox`.`usuario` (`nombre`, `apellidos`, `correo`, `contrasena`, `idTipo`) VALUES (?, ?, ?, ?, ?);";
         try (PreparedStatement statement = conexion.prepareStatement(query)) {
             statement.setString(1, usuario.getNombre());
-            statement.setString(2, usuario.getContrasena());
+            statement.setString(2, usuario.getApellidos());
             statement.setString(3, usuario.getCorreo());
-            statement.setInt(4, usuario.getIdTipo());
+            statement.setString(4, usuario.getContrasena());
+            statement.setInt(5, usuario.getIdTipo());
             statement.executeUpdate();
         }
     }
 
     @Override
     public void updateUsuario(Usuario usuario) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String query = "UPDATE `tutobox`.`usuario` SET `nombre` = ?, `apellidos` = ?, `correo` = ?, `contrasena` = ?, `idTipo` = ? WHERE (`idUsuario` = ?);";
+        try (PreparedStatement statement = conexion.prepareStatement(query)) {
+            statement.setString(1, usuario.getNombre());
+            statement.setString(2, usuario.getApellidos());
+            statement.setString(3, usuario.getCorreo());
+            statement.setString(4, usuario.getContrasena());
+            statement.setInt(5, usuario.getIdTipo());
+            statement.setInt(6, usuario.getIdUsuario());
+            statement.executeUpdate();
+        }
     }
 
     @Override
     public void deleteUsuario(Usuario usuario) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         String query = "DELETE FROM `tutobox`.`usuario` WHERE (`idUsuario` = ?);";
+        try (PreparedStatement statement = conexion.prepareStatement(query)) {
+            statement.setInt(1, usuario.getIdUsuario());
+            statement.executeUpdate();
+        }   
     }
 
     @Override
     public List<Usuario> listar() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<Usuario> listaUsuario = new ArrayList<>();
+        String query = "SELECT * FROM tutobox.usuario;";
+        try (PreparedStatement statement = conexion.prepareStatement(query)) {
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()){
+                int id=rs.getInt(1);
+                String nombre=rs.getString(2);
+                String apellidos=rs.getString(3);
+                String correo=rs.getString(4);
+                String password=rs.getString(5);
+                int idTipo = rs.getInt(6);
+                Usuario usu=new Usuario(id, nombre, apellidos, password, correo, idTipo);
+                listaUsuario.add(usu);
+            }
+        } 
+        return listaUsuario;
     }
 
 
--- TUTOBOX/src/main/java/test/testUsuario.java
+++ TUTOBOX/src/main/java/test/testUsuario.java
@@ -4,6 +4,14 @@
  */
 package test;
 
+import dao.UsuarioDAOImpl;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Usuario;
+
 /**
  *
  * @author Brayan
@@ -11,7 +19,51 @@ package test;
 public class testUsuario {
     
     public static void main (String [] args){
-        System.out.println("prueba");
+        
+//        //test para insersion
+//        Usuario usuario = new Usuario(0,"pierina", "bustamante", "123456", "brayan@gmail.com", 2);
+//        UsuarioDAOImpl usudao = new UsuarioDAOImpl();
+//        try {
+//            usudao.insertarUsuario(usuario);
+//        } catch (SQLException ex) {
+//            System.out.println("erro al insertar "+ex);
+//        }
+        
+
+        //test modificar
+//        Usuario usuario = new Usuario(12,"p", "b", "1", "b", 1);
+//        UsuarioDAOImpl usudao = new UsuarioDAOImpl();
+//        try {
+//            usudao.updateUsuario(usuario);
+//        } catch (SQLException ex) {
+//            System.out.println("erro al insertar "+ex);
+//        }
+        
+    
+        //test eliminar usuario
+//        Usuario usuario = new Usuario(12,"p", "b", "1", "b", 1);
+//        UsuarioDAOImpl usudao = new UsuarioDAOImpl();
+//        try {
+//            usudao.deleteUsuario(usuario);
+//        } catch (SQLException ex) {
+//            System.out.println("erro al insertar "+ex);
+//        }
+        
+        UsuarioDAOImpl usudao = new UsuarioDAOImpl();
+        List<Usuario> listaUsuarios = new ArrayList<>();
+        try {
+            listaUsuarios=usudao.listar();
+            for (Usuario u : listaUsuarios) {
+                System.out.println("ID : "+u.getIdUsuario());
+                System.out.println("Nombre : "+u.getNombre());
+                System.out.println("Apellido : "+u.getApellidos());
+                System.out.println("Correo : "+u.getCorreo());
+                System.out.println("Contraseña : "+u.getContrasena());
+                System.out.println("IDTipo : "+u.getIdTipo());
+            }
+        } catch (SQLException ex) {
+            System.out.println("erro al listar "+ex);
+        }
     }
     
 }
